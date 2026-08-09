317d01786844f125f8df89a895ebd37562c61b66
rahmtirahman434-sketch <rahmtirahman434@gmail.com>
2026-08-09 13:32:15 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 
+from kivy.app import App
+from kivy.uix.label import Label
+
+class RahmanApp(App):
+    def build(self):
+        return Label(text='رحمان رحمتی', font_size=32)
+
+RahmanApp().run()
