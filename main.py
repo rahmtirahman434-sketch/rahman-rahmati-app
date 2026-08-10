4970eba36f5fa4f775bee31cf0207e9c8882f5b1
rahmtirahman434-sketch <rahmtirahman434@gmail.com>
2026-08-10 09:21:01 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.core.text import LabelBase
+
+LabelBase.register(name="Vazir", fn_regular="Vazirmatn-Regular (1).ttf")
 
 class RahmanApp(App):
     def build(self):
-        return Label(text='رحمان رحمتی', font_size=32)
+        return Label(text='رحمان رحمتی', font_size=32, font_name="Vazir")
 
 RahmanApp().run()
